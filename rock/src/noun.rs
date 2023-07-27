d8f77314595a68e11ebc0c6a8e57966bd25ee263
Felix Watts <felixwatts@gmail.com>
2023-07-27 19:10:09 +0100

    refactor

---
+++ rock/src/noun.rs
@@ -0,0 +1,38 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::rc::Rc;
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Cell {
+    pub head: Noun,
+    pub tail: Noun,
+}
+
+#[derive(Eq, PartialEq, Clone)]
+pub enum Noun {
+    Atom(u32),
+    Cell(Rc<Cell>),
+}
+
+pub(crate) fn cell(head: Noun, tail: Noun) -> Noun {
+    Noun::Cell(Rc::new(Cell { head, tail }))
+}
+
+pub(crate) fn atom(val: u32) -> Noun {
+    Noun::Atom(val)
+}
+
+impl Debug for Noun {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Noun::Atom(val) => write!(f, "{}", val),
+            Noun::Cell(cell) => write!(f, "[{} {}]", cell.head, cell.tail),
+        }
+    }
+}
+
+impl Display for Noun {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
---
+++ rock/src/parse.rs
@@ -0,0 +1,77 @@
+use crate::noun::Cell;
+use crate::noun::Noun;
+use nom::bytes::complete::take_while;
+use nom::multi::many_m_n;
+use nom::Parser;
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::one_of, multi::many0,
+    sequence::delimited, IResult,
+};
+use std::rc::Rc;
+
+impl TryFrom<&str> for Noun {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let (remaining, noun) = parse_noun(value).map_err(|e| e.to_string())?;
+        if !remaining.is_empty() {
+            Err("Expected end of input".into())
+        } else {
+            Ok(noun)
+        }
+    }
+}
+
+fn parse_noun(input: &str) -> IResult<&str, Noun> {
+    let (remaining_input, _) = take_while(|c: char| c.is_whitespace())(input)?;
+    alt((parse_atom, parse_list))(remaining_input)
+}
+
+fn parse_atom(input: &str) -> IResult<&str, Noun> {
+    let (remaining_input, decimal_chars) = many_m_n(1, 1024, one_of("0123456789")).parse(input)?;
+    let decimal_str: String = decimal_chars.into_iter().collect();
+    let val = u32::from_str_radix(&decimal_str, 10).unwrap();
+    Ok((remaining_input, Noun::Atom(val)))
+}
+
+fn parse_list(input: &str) -> IResult<&str, Noun> {
+    let mut parse = delimited(tag("["), many0(parse_noun), tag("]"));
+    let (remaining_input, nouns) = parse(input)?;
+    let cell = to_cell(&nouns);
+    Ok((remaining_input, Noun::Cell(Rc::new(cell))))
+}
+
+fn to_cell(nouns: &[Noun]) -> Cell {
+    match nouns.len() {
+        2 => Cell {
+            head: nouns[0].clone(),
+            tail: nouns[1].clone(),
+        },
+        _ => Cell {
+            head: nouns[0].clone(),
+            tail: Noun::Cell(Rc::new(to_cell(&nouns[1..]))),
+        },
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::noun::{atom, cell};
+
+    use super::*;
+
+    #[test]
+    fn test_parse_noun() {
+        let (remaining_input, ast) = parse_noun("1").unwrap();
+        assert_eq!("", remaining_input);
+        assert_eq!(Noun::Atom(1), ast);
+
+        let (remaining_input, ast) = parse_noun("[1 2]").unwrap();
+        assert_eq!("", remaining_input);
+        assert_eq!(cell(atom(1), atom(2)), ast);
+
+        let (remaining_input, ast) = parse_noun("[1 2 3]").unwrap();
+        assert_eq!("", remaining_input);
+        assert_eq!(cell(atom(1), cell(atom(2), atom(3))), ast);
+    }
+}
